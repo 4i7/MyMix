5ad7896279b3ad9adaa96a3116fb8c9e2cc79057
4i7 <3959289+4i7@users.noreply.github.com>
2026-08-15 13:48:23 +0900

    Expose mute and output cycle hotkeys

--- EarTrumpet/UI/ViewModels/EarTrumpetShortcutsPageViewModel.cs
+++ EarTrumpet/UI/ViewModels/EarTrumpetShortcutsPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using EarTrumpet.Interop.Helpers;
+using EarTrumpet.Interop.Helpers;
+using System.Globalization;
 
 namespace EarTrumpet.UI.ViewModels
 {
@@ -21,16 +22,27 @@ namespace EarTrumpet.UI.ViewModels
         public HotkeyViewModel AbsoluteVolumeDownHotkey { get; }
         public string DefaultAbsoluteVolumeDownHotkey => s_hotkeyNoneText;
 
+        public HotkeyViewModel ToggleMuteHotkey { get; }
+        public string DefaultToggleMuteHotkey => s_hotkeyNoneText;
+
+        public HotkeyViewModel CycleOutputDeviceHotkey { get; }
+        public string DefaultCycleOutputDeviceHotkey => s_hotkeyNoneText;
+
+        public string ToggleMuteText => Properties.Resources.ResourceManager.GetString("SettingsToggleMuteHotkeyText", CultureInfo.CurrentUICulture) ?? "Toggle default output mute";
+        public string CycleOutputDeviceText => Properties.Resources.ResourceManager.GetString("SettingsCycleOutputDeviceHotkeyText", CultureInfo.CurrentUICulture) ?? "Switch to next playback device";
+
         public EarTrumpetShortcutsPageViewModel(AppSettings settings) : base(null)
         {
             Title = Properties.Resources.ShortcutsPageText;
             Glyph = "\xE765";
 
-            OpenFlyoutHotkey = new HotkeyViewModel(settings.FlyoutHotkey, (newHotkey) => settings.FlyoutHotkey = newHotkey);
-            OpenMixerHotkey = new HotkeyViewModel(settings.MixerHotkey, (newHotkey) => settings.MixerHotkey = newHotkey);
-            OpenSettingsHotkey = new HotkeyViewModel(settings.SettingsHotkey, (newHotkey) => settings.SettingsHotkey = newHotkey);
-            AbsoluteVolumeUpHotkey = new HotkeyViewModel(settings.AbsoluteVolumeUpHotkey, (newHotkey) => settings.AbsoluteVolumeUpHotkey = newHotkey);
-            AbsoluteVolumeDownHotkey = new HotkeyViewModel(settings.AbsoluteVolumeDownHotkey, (newHotkey) => settings.AbsoluteVolumeDownHotkey = newHotkey);
+            OpenFlyoutHotkey = new HotkeyViewModel(settings.FlyoutHotkey, newHotkey => settings.FlyoutHotkey = newHotkey);
+            OpenMixerHotkey = new HotkeyViewModel(settings.MixerHotkey, newHotkey => settings.MixerHotkey = newHotkey);
+            OpenSettingsHotkey = new HotkeyViewModel(settings.SettingsHotkey, newHotkey => settings.SettingsHotkey = newHotkey);
+            AbsoluteVolumeUpHotkey = new HotkeyViewModel(settings.AbsoluteVolumeUpHotkey, newHotkey => settings.AbsoluteVolumeUpHotkey = newHotkey);
+            AbsoluteVolumeDownHotkey = new HotkeyViewModel(settings.AbsoluteVolumeDownHotkey, newHotkey => settings.AbsoluteVolumeDownHotkey = newHotkey);
+            ToggleMuteHotkey = new HotkeyViewModel(settings.ToggleMuteHotkey, newHotkey => settings.ToggleMuteHotkey = newHotkey);
+            CycleOutputDeviceHotkey = new HotkeyViewModel(settings.CycleOutputDeviceHotkey, newHotkey => settings.CycleOutputDeviceHotkey = newHotkey);
         }
     }
 }
